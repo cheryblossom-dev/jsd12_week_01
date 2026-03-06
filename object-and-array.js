53d211a9183f021bf6392476aba9238504ec7cbd
cheryblossom-dev <kaaashaaa9@gmail.com>
2026-03-06 14:07:42 +0700

    add object-and-array.js

---
+++ object-and-array.js
@@ -0,0 +1,6 @@
+let learner={name:"happy",age:3,isGraduated:false,hobbies:["football","music","gaming"]};
+
+console.log(learner.name);
+console.log(learner.age);
+console.log(learner.isGraduated);
+console.log(learner.hobbies);
